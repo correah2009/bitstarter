4867e22a602848cc9ff731894b7149cced84ab93
Hector Correa <correah2009@gmail.com>
2013-07-11 04:39:23 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var file = fs.readFileSync(index.html);
+    var num= buffer.write(fs.readFileSync(index.html, encoding), "utf-8");
+    var response.send(buffer.toString("utf-8",0,num));
 });
 
 var port = process.env.PORT || 5000;
